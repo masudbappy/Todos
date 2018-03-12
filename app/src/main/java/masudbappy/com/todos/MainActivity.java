4e2b635bc9d3abafa251465057d351b5ff7d4408
masudbappy <masud.bappy90@gmail.com>
2018-03-12 18:46:23 +0600

    updated xml

--- app/src/main/java/masudbappy/com/todos/MainActivity.java
+++ app/src/main/java/masudbappy/com/todos/MainActivity.java
@@ -1,5 +1,6 @@
 package masudbappy.com.todos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] itemname = {
+      "Get theaters tickets",
+            "Order pizza for tonight",
+            "Buy groceries",
+            "Running session at 19.30",
+            "Call Uncle Sam"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final ListView lv = (ListView) findViewById(R.id.lvTodos);
+        lv.setAdapter(new ArrayAdapter<String>(this, R.layout.todos_list_item,
+                    R.id.tvNote, itemname));
+        //adds the click event to the listView, reading the content
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                Intent intent = new Intent(MainActivity.this, TodoActivity.class);
+                String content = (String) lv.getItemAtPosition(pos);
+                intent.putExtra("Content",content);
+                startActivity(intent);
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/masudbappy/com/todos/TodoActivity.java
+++ app/src/main/java/masudbappy/com/todos/TodoActivity.java
@@ -1,7 +1,9 @@
 package masudbappy.com.todos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class TodoActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TodoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+
+        Intent intent = getIntent();
+        String content = intent.getStringExtra("Content");
+        EditText editToDo = (EditText) findViewById(R.id.editTodo);
+        editToDo.setText(content);
     }
 }
